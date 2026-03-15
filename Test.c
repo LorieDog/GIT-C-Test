504d2e70ecf57ea67504e68e15b85488c052af77
LorieDog <Doggolory@gmail.com>
2026-03-15 19:22:25 +0100

    Multi Thread Test

--- Test.c
+++ Test.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *codice_thread(void *arg)
+{
+	pid_t pid;
+	pid = getpid();
+	pthread_t tid;
+	tid = pthread_self();
+	printf("Sono il thread %i, del processo %i!\n", tid, pid);
+}
 
 int main(void)
 {
-	int a;
-	printf("Inserire input: \n");
-	scanf("%d", &a);
-	printf("L'input che ai inserito è: %d\n", a);
+	pthread_t ptid;
+	printf("Il thread del main è: %d\n", getpid());
+	pthread_create (&ptid, NULL, &codice_thread, NULL);
 	return 0;
 }
